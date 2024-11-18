a5f1c8fed411ca906081b43d39fa64a058823dea
Abu Osama <s.islam20000@gmail.com>
2024-11-18 06:41:47 +0530

    final commit

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Tasks",
-  password: "Saifi2323#",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool
